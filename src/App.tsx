e086d58ce9801c039f64ee9d88d82de14309d197
Vollttz <itamar1zohar@gmail.com>
2025-10-16 08:32:27 -0700

    Project setup (#13)
    * Project setup + loginForm visu
    
    configured the project using Vite and setup the purely visual part of the login form
    
    * forgot to delete stuff
    
    unnecessary css styling that came with project setup being deleted

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import LoginForm from "./Components/LoginForm/LoginForm";
+
+function App() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
+
+export default App;
